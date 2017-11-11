20b574535ff3418d874c7cb253bb9097330a7f02
XueGuoliang <xueguoliang@Ivan-MacBook-Air.local>
2017-11-11 14:40:48 +0100

    preprocessing done

--- preprocessing.py
+++ preprocessing.py
@@ -1,29 +1,33 @@
 import json
 from utils import parser
-
-rows = []
-
-first = True
-
-parser = parser.Parser('../data/bitcoin-bitcoin-commits.logs')
+import glob
+import os
+from tqdm import tqdm
 
 f = open('features.csv', 'a')
 f.write("name" + "," + "commit_count" + "," + "star_count" + "," + "watcher_count" + "," + "forks_count" + "," + "contributers_count" + "," + "open_issues_count" + "," + "created_time"+"\n")
 
-with open('/Users/xueguoliang/gitchain/data/bitcoin-bitcoin.json') as json_file:
-    record = json.load(json_file)
+for coin in tqdm(glob.glob("data/*.json")):
 
-    # get the feature from raw data
-    row = str(record["name"]) + "," + \
-          str(parser.count_commits()) + "," + \
-          str(record["stargazers_count"]) + "," + \
-          str(record["watchers_count"]) + "," + \
-          str(record["forks_count"]) + "," + \
-          str(len(record["contributors"])) + "," + \
-          str(record["open_issues_count"]) + "," + \
-          str(record["created_at"])
+    basename = os.path.splitext(coin)[0].split("/")[-1]
 
-    f.write(row + "\n")  # python will convert \n to os.linesep
+    log = parser.Parser("../data/"+basename+"-commits.logs")
+    commit_count = log.count_commits()
+
+    with open(coin) as json_file:
+        record = json.load(json_file)
+
+        # get the feature from raw data
+        row = str(record["name"]) + "," + \
+              str(commit_count) + "," + \
+              str(record["stargazers_count"]) + "," + \
+              str(record["watchers_count"]) + "," + \
+              str(record["forks_count"]) + "," + \
+              str(len(record["contributors"])) + "," + \
+              str(record["open_issues_count"]) + "," + \
+              str(record["created_at"])
+
+        f.write(row + "\n")  # python will convert \n to os.linesep
 
 f.close()
 
