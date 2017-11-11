5ce221803ae4947e9bff0e8584564f731fd55fa6
XueGuoliang <xueguoliang@Ivan-MacBook-Air.local>
2017-11-11 13:59:19 +0100

    fix encoding and save problem

--- preprocessing.py
+++ preprocessing.py
@@ -3,29 +3,27 @@ from utils import parser
 
 rows = []
 
-parser = parser.Parser('../data/bitcoin-bitcoin-commits.logs')
-print(parser.count_commits())
+first = True
 
+parser = parser.Parser('../data/bitcoin-bitcoin-commits.logs')
+
+f = open('features.csv', 'a')
+f.write("name" + "," + "commit_count" + "," + "star_count" + "," + "watcher_count" + "," + "forks_count" + "," + "contributers_count" + "," + "open_issues_count" + "," + "created_time"+"\n")
 
 with open('/Users/xueguoliang/gitchain/data/bitcoin-bitcoin.json') as json_file:
     record = json.load(json_file)
 
     # get the feature from raw data
-    row = str(record["id"]) + "," + \
+    row = str(record["name"]) + "," + \
+          str(parser.count_commits()) + "," + \
           str(record["stargazers_count"]) + "," + \
           str(record["watchers_count"]) + "," + \
           str(record["forks_count"]) + "," + \
-          str(record["watchers"]) + "," + \
-          str(len(record["contributors"])) + "," + \
           str(len(record["contributors"])) + "," + \
           str(record["open_issues_count"]) + "," + \
-          str(record["created_at"]) + "," + \
-          str(record["size"])
+          str(record["created_at"])
 
-    rows.append(row)
+    f.write(row + "\n")  # python will convert \n to os.linesep
 
-print(rows)
-
-
-
+f.close()
 
